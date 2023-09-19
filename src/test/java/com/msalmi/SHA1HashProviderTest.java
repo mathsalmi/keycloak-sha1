43d9a2275afef6abfd33e6c02a7a7802e87b21d2
mrbrunelli <matheus.brunelli@gmail.com>
2023-09-19 17:56:28 -0300

    refactor tests and add new test cases

--- src/test/java/com/msalmi/SHA1HashProviderTest.java
+++ src/test/java/com/msalmi/SHA1HashProviderTest.java
@@ -1,44 +1,61 @@
 package com.msalmi;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SHA1HashProviderTest {
+	private SHA1HashProvider provider;
+
+	@BeforeEach
+	public void setup() {
+		provider = new SHA1HashProvider(SHA1HashProviderFactory.ID);
+	}
 
 	@Test
-	public void encodeHelloWorld() {
-		final var provider = new SHA1HashProvider(SHA1HashProviderFactory.ID);
-		var expected = "2aae6c35c94fcfb415dbe95f408b9ce91ee846ed";
+	@DisplayName("Should encode 'hello world'")
+	public void shouldEncodeHelloWorld() {
+		var expected = "a67ee490ee7bff6898131cdc31e534338103c095";
 		var encoded = provider.encode("hello world", 0);
-		assertTrue(encoded.equals(expected));
+
+		assertEquals(expected, encoded);
+	}
+
+	@Test
+	@DisplayName("Should encode 'hello world' and ignore iterations")
+	public void shouldEncodeHelloWorldAndIgnoreIterations() {
+		var expected = "a67ee490ee7bff6898131cdc31e534338103c095";
+		var encoded = provider.encode("hello world", 12);
+
+		assertEquals(expected, encoded);
 	}
 	
 	@Test
-	public void encodeEmptyString() {
-		final var provider = new SHA1HashProvider(SHA1HashProviderFactory.ID);
+	@DisplayName("Should encode empty string")
+	public void shouldEncodeEmptyString() {
 		var expected = "da39a3ee5e6b4b0d3255bfef95601890afd80709";
 		var encoded = provider.encode("", 0);
-		assertTrue(encoded.equals(expected));
+
+		assertEquals(expected, encoded);
 	}
-	
+
 	@Test
-	public void ensureIterationParameterIsIgnored() {
-		final var provider = new SHA1HashProvider(SHA1HashProviderFactory.ID);
-		var expected = "da39a3ee5e6b4b0d3255bfef95601890afd80709";
-		var encoded = provider.encode("", 0);
-		assertTrue(encoded.equals(expected));
-		
-		expected = "da39a3ee5e6b4b0d3255bfef95601890afd80709";
-		encoded = provider.encode("", 42); // any random number
-		assertTrue(encoded.equals(expected));
+	@DisplayName("Should encode with leading zeros")
+	public void shouldEncodeWithLeadingZeros() {
+		var expected = "0dd8e518cc8b6e0e4344462b1bab04dd5fe263da";
+		var encoded = provider.encode("At4zpr9csm", 0);
+
+		assertEquals(expected, encoded);
 	}
-	
+
 	@Test
-	public void testHashesWithLeadingZeros() {
-		final var provider = new SHA1HashProvider(SHA1HashProviderFactory.ID);
-		var expected = "042dc4512fa3d391c5170cf3aa61e6a638f84342";
-		var encoded = provider.encode("i", 0);
-		assertTrue(encoded.equals(expected));
+	@DisplayName("Should encode random string")
+	public void shouldEncodeRandomString() {
+		var expected = "e17d7c8ca045548d3bf7ad9eeb2f7ecf83654e78";
+		var encoded = provider.encode("Zt72864b5", 0);
+
+		assertEquals(expected, encoded);
 	}
 }
